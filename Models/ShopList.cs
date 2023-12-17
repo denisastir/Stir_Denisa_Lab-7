c3b988f6a4d6d6771f638ab9f7d25cf82c2f69ef
denisastir <151443506+denisastir@users.noreply.github.com>
2023-12-17 22:31:56 +0200

    Add files via upload

---
+++ Models/ShopList.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+namespace Stir_Denisa_Lab_7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
